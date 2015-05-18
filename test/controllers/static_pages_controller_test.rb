17d85e24d9763932c90e3d5bc7f09126a6af4603
Kollo David <kdavid77@gmail.com>
2015-05-18 09:04:17 +0300

    first commit

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | My Control Panel"
+  end
+
+  test "should get help" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | My Control Panel"
+  end
+
+end
