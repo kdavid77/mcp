ca8ead832b0dfaa1634f948bc1c5dce4538df368
Kollo David <kdavid77@gmail.com>
2015-05-19 21:28:33 +0300

    first helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | My Control Panel"
+    assert_select "title", "My Control Panel"
   end
 
   test "should get help" do
