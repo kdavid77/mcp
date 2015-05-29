2fc5ab2b09326e42129f4c1d8f63b72bc8c24656
Kollo David <kdavid77@gmail.com>
2015-05-29 08:41:39 +0300

    mindenfele static_pages javitgatas

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,17 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "My Control Panel"
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get :contact
+    get :help
     assert_response :success
-    assert_select "title", "Contact | My Control Panel"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
